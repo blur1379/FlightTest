31e1e960e2496799e2b51c2185f9ec2c5a61ed21
blur1379 <77322707+blur1379@users.noreply.github.com>
2023-02-17 01:07:36 +0330

    Initial Commit

---
+++ FlightTest/FlightTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightTestApp.swift
+//  FlightTest
+//
+//  Created by Mohammad Blur on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct FlightTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
